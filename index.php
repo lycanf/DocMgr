e92c3a037e55d9b97aee3204096970d919efa677
lycanf <fangking1988@gmail.com>
2015-04-30 00:15:51 +0800

    update

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+	include 'hello/libcls.php';
+
 	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
 		$uri = 'https://';
 	} else {
@@ -8,5 +10,11 @@
 	echo $uri;
 	file_put_contents("E:/log.txt","打印操作\r\n",FILE_APPEND);
 	file_put_contents("E:/log.txt",$uri,FILE_APPEND);
+	
+	echo "</br>";
+	$h = new \hello\Hello("fff",300);
+	$h->sayHello();
+	
+	$m = new \hello\MyHello('qqq');
 ?>
 
